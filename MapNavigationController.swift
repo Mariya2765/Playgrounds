c7e3bc2aba7d597a42c37175a2465614fdd9ad75
Mariya Chernuhina <mariya.chernuhina@mail.ru>
2023-05-17 22:21:58 +0300

    add createTabBarController, createSearchViewController, createMapNavigationController

---
+++ MapNavigationController.swift
@@ -0,0 +1,16 @@
+//
+//  1234.swift
+//  Playgrounds
+//
+//  Created by Мария on 15.05.2023.
+//
+
+import UIKit
+
+class MapNavigationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ SearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  createSearchViewController.swift
+//  Playgrounds
+//
+//  Created by Мария on 17.05.2023.
+//
+
+import UIKit
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
