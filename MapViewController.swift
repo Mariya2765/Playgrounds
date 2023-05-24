49caf68be697d99de6c3c849fb0902019e91cc1b
Mariya Chernuhina <mariya.chernuhina@mail.ru>
2023-05-24 14:42:29 +0300

    add Map

--- MapViewController.swift
+++ MapViewController.swift
@@ -6,11 +6,36 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
+
+    let mapView: MKMapView = {
+        let map = MKMapView()
+        // вид интерфейса темный
+        map.overrideUserInterfaceStyle = .dark
+        map.translatesAutoresizingMaskIntoConstraints = false
+
+        return map
+    }()
+//    let locationManager = CLLocationManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        view.addSubview(mapView)
+        setMapConstreints()
+    }
+
+    func setMapConstreints() {
+        NSLayoutConstraint.activate([
+
+            mapView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            mapView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+            ])
     }
 
 }
